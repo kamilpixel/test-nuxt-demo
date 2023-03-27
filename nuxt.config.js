779b1e641584392ec5bb6fed115f4abea22ab7bb
Kamil <48324735+kamilpixel@users.noreply.github.com>
2023-03-27 09:49:08 +0800

    deploy scripts

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: (process.env.NODE_ENV === 'production') ? '/test-nuxt-demo/' : '/',
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'test-nuxt-demo',
